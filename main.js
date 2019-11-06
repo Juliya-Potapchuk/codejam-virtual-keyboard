690e7b00fc8d83aa7e894bff8eb8f22bd2818e59
Juliya Potapchuk <potapchuk.julya@list.ru>
2019-11-06 21:33:59 +0300

    feat: add textarea/keyboard

---
+++ main.js
@@ -0,0 +1,8 @@
+const textarea = document.createElement('textarea');
+textarea.setAttribute('rows', 15);
+textarea.setAttribute('cols', 100);
+document.body.append(textarea);
+
+const keyboard = document.createElement('div');
+keyboard.classList.add('keyboard');
+document.body.append(keyboard);
